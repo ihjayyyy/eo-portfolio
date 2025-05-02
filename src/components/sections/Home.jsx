032b34edc96ce0e38adbeafaa81d1d19e2217456
Elbert John Olivar <79898747+ihjayyyy@users.noreply.github.com>
2025-05-02 18:22:53 +0800

    arrow adjust

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -75,7 +75,7 @@ function Home() {
           </div>
         </div>
       </RevealOnScroll>
-      <div className="absolute bottom-10 animate-bounce">
+      <div className="absolute bottom-0 animate-bounce">
         <svg className="w-6 h-6 text-sky-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
         </svg>
